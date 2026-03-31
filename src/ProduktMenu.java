c4f647073310e18c304eac2032656a4517a1debc
Lena Lewkowicz <s30126@pjwstk.edu.pl>
2026-03-31 23:14:15 +0200

    klasa ProduktMenu

--- src/ProduktMenu.java
+++ src/ProduktMenu.java
@@ -1,10 +1,21 @@
+import java.util.Objects;
 public class ProduktMenu {
 
     private String kod;
     private String nazwa;
     private double cena;
     private String kategoria;
-    private static int liczbaProduktow;
+    private static int liczbaProduktow = 0;
+
+    public ProduktMenu(String kod, String nazwa, double cena, String kategoria) {
+
+        this.kod = kod;
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.kategoria = kategoria;
+        liczbaProduktow++;
+
+    }
 
     public String getKod() {
         return kod;
@@ -26,13 +37,14 @@ public class ProduktMenu {
         return liczbaProduktow;
     }
 
-    public ProduktMenu(String kod, String nazwa, double cena, String kategoria) {
+    public String toString() {
+        return this.nazwa + "\n" + this.kategoria + "\n" + this.cena + "\n" + this.kod + "\n";
+    }
 
-        this.kod = kod;
-        this.nazwa = nazwa;
-        this.cena = cena;
-        this.kategoria = kategoria;
-
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ProduktMenu produkt = (ProduktMenu) o;
+        return Objects.equals(kod, produkt.kod);
     }
 
 }
